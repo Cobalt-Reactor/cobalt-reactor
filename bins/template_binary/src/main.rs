3d29cf01cebde7245bba2716c03ef613b69aadbe
Bob Gardner <rgardner@inworld.ai>
2024-07-25 09:13:38 -0700

    Adding template plugin comment to template binary

--- bins/template_binary/src/main.rs
+++ bins/template_binary/src/main.rs
@@ -2,3 +2,33 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Template plugin
+// use bevy::prelude::*;
+//
+// pub struct TemplatePlugin;
+//
+// impl Plugin for TemplatePlugin {
+//     fn build(&self, app: &mut App) {
+//         self.add_events(app);
+//         self.add_plugins(app);
+//         self.register_types(app);
+//         self.insert_resources(app);
+//         self.add_systems(app);
+//         self.configure_sets(app);
+//     }
+// }
+//
+// impl TemplatePlugin {
+//     fn add_events(&self, _: &mut App) {}
+//
+//     fn add_plugins(&self, _: &mut App) {}
+//
+//     fn register_types(&self, _: &mut App) {}
+//
+//     fn insert_resources(&self, _: &mut App) {}
+//
+//     fn add_systems(&self, _: &mut App) {}
+//
+//     fn configure_sets(&self, _: &mut App) {}
+// }
