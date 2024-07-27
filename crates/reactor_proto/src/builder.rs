19c7aff099e9b9b63ea2651bc0ba7ed7746a21b7
Bob Gardner <rgardner@inworld.ai>
2024-07-27 14:46:04 -0700

    feat: Changed EntityBuilder to install modules immediately, and no longer requires you to call build

--- crates/reactor_proto/src/builder.rs
+++ crates/reactor_proto/src/builder.rs
@@ -1,36 +1,56 @@
 use crate::module::Module;
-use bevy::prelude::*;
+use bevy::{
+    ecs::system::{EntityCommand, EntityCommands},
+    prelude::*,
+};
 
 /// An entity builder, used to build entities from modules
-#[derive(Default)]
-pub struct EntityBuilder {
-    modules: Vec<Box<dyn Module>>,
+pub struct EntityBuilder<'a> {
+    target: &'a mut EntityBuilderTarget<'a>,
 }
 
-impl EntityBuilder {
+pub enum EntityBuilderTarget<'a> {
+    EntityCommands(&'a mut EntityCommands<'a>),
+    EntityWorldMut(&'a mut EntityWorldMut<'a>),
+}
+
+impl<'a> EntityBuilder<'a> {
     /// Creates a new entity builder
-    pub fn new() -> Self {
-        Default::default()
+    pub fn new(target: &'a mut EntityBuilderTarget<'a>) -> Self {
+        Self { target }
     }
 
-    /// Adds a module to the builder
-    pub fn with<M: Module + 'static>(mut self, module: M) -> Self {
-        self.modules.push(Box::new(module));
+    /// Adds a module to the associated entity
+    pub fn install<M: Module + 'static>(&mut self, module: M) -> &Self {
+        match &mut self.target {
+            EntityBuilderTarget::EntityCommands(ref mut ec) => {
+                ec.add(move |mut ewm: EntityWorldMut| module.install(&mut ewm));
+            }
+            EntityBuilderTarget::EntityWorldMut(ref mut ewm) => {
+                module.install(ewm);
+            }
+        };
+
         self
     }
 
-    /// Installs all added modules on the target
-    pub fn build(self, target: &mut EntityWorldMut) {
-        for module in self.modules {
-            module.install(target);
-        }
-    }
-
-    /// Updates all added modules, useful for hot reloading
+    /// Adds a module to the builder
     #[cfg(feature = "hot_reload")]
-    pub fn rebuild(self, target: &mut EntityWorldMut) {
-        for module in self.modules {
-            module.update(target);
-        }
+    pub fn update<M: Module + 'static>(&mut self, module: M) -> &Self {
+        match &mut self.target {
+            EntityBuilderTarget::EntityCommands(ref mut ec) => {
+                ec.add(move |mut ewm: EntityWorldMut| module.update(&mut ewm));
+            }
+            EntityBuilderTarget::EntityWorldMut(ref mut ewm) => {
+                module.update(ewm);
+            }
+        };
+
+        self
     }
 }
+
+// TODO: `entity_builder` extension for EntityCommands and the CommandQueue equiv that
+// returns an entity builder for the given entity
+// TODO: Create a common modules crate for many of the modules in Zombies (and refactor hearing and
+// vision to have optional Fixed or Unlimited range)
